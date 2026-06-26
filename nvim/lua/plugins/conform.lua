459c16ecd7409bd328e321de1003a30b93d0daa1
mueezejaz <mueezejaz12345678@gmail.com>
2026-06-26 11:13:55 +0500

    update nvim config

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,32 +1,32 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = "BufWritePre",
-    keys = {
-      {
-        "<leader>f",
-        function()
-          require("conform").format({ async = true, lsp_fallback = true })
-        end,
-        mode = "",
-        desc = "Format buffer",
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      format_on_save = { lsp_fallback = true, timeout_ms = 200 },
-      formatters_by_ft = {
-        lua = { "stylua" },
-        c = {},
-        cpp = {},
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        vue = { "prettier" },
-        css = { "prettier" },
-        json = { "prettier" },
-        html = { "prettier" },
-        markdown = { "prettier" },
-      },
-    },
-  },
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre",
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		opts = {
+			notify_on_error = false,
+			format_on_save = { lsp_fallback = true, timeout_ms = 2000 },
+			formatters_by_ft = {
+				lua = { "stylua" },
+				c = {},
+				cpp = {},
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				vue = { "prettier" },
+				css = { "prettier" },
+				json = { "prettier" },
+				html = { "prettier" },
+				markdown = { "prettier" },
+			},
+		},
+	},
 }
